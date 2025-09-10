d0bfba93e5fb6e7b11989c69d6be55660bd1fe75
madotto.Axel@outlook.com <madotto.axel@outlook.com>
2025-09-10 15:34:05 +0200

    change user integration test

--- src/users/users.spec.ts
+++ src/users/users.spec.ts
@@ -1,39 +1,52 @@
+import { INestApplication } from '@nestjs/common';
 import { createTestingModule } from '../../test/utils/test-utils';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TestingModule } from '@nestjs/testing';
+import request from 'supertest';
 
 describe('Users', () => {
   let service: UsersService;
   let controller: UsersController;
   let module: TestingModule;
+  let application: INestApplication;
 
   beforeAll(async () => {
-    module = await createTestingModule([UsersService, UsersController]);
+    module = await createTestingModule([UsersService], [UsersController]);
     controller = module.get<UsersController>(UsersController);
     service = module.get<UsersService>(UsersService);
-    
+    application = module.createNestApplication();
+    await application.init();
+  });
+
+  afterAll(() => {
+    application.close();
   });
 
   describe('createUser', () => {
+    const item = {
+      name: 'A',
+      email: 'at@afz.com',
+      password: 'test',
+    };
     it('should add a new user into the database', async () => {
-      const newItem = await controller.createUser({
-        name: 'A',
-        email: 'at@afz.com',
-        password: 'test',
-      });
-      expect(newItem.id).toBeDefined();
-      expect(newItem.name).toBe('A');
-      const dbUser = await service.findOneById(1)
-      expect(dbUser).toEqual(newItem)
+      const response = await request(application.getHttpServer())
+        .post('/users')
+        .send(item)
+        .expect(201);
+      const newUser = response.body;
+      expect(newUser.id).toBeDefined();
+      expect(newUser.name).toBe(item.name);
+      const dbUser = await service.findOneById(newUser.id);
+      expect(dbUser).toMatchObject({...newUser , created_at : new Date(newUser.created_at)});
     });
 
     it('should have a crypt password', async () => {
-      const newItem = await controller.createUser({
-        name: 'A',
-        email: 'at@afz.com',
-        password: 'test',
-      });
+      const response = await request(application.getHttpServer())
+        .post('/users')
+        .send(item)
+        .expect(201);
+        const newItem = response.body
       expect(newItem.password).not.toBe('test');
     });
   });
