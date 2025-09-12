5098559a6c99e75631d39673ad34385cee37add4
madotto.Axel@outlook.com <madotto.axel@outlook.com>
2025-09-12 10:31:21 +0200

    test with pg

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { InitialModule } from './initial.module';
 import { ValidationPipe } from '@nestjs/common';
 
+import { Client } from 'pg';
+
 async function bootstrap() {
+  
+
+  // Test de connexion à la DB
+  const client = new Client({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false },
+    family: 4, // force IPv4
+  });
+
+  try {
+    await client.connect();
+    console.log('✅ DB Connected!');
+  } catch (err) {
+    console.error('❌ DB Connection error:', err);
+  } finally {
+    await client.end();
+  }
   const app = await NestFactory.create(InitialModule);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
